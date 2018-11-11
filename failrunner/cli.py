7b9d1bd1502bacef33174fbde18137eb7f046378
Harry White <harry.white424@gmail.com>
2018-11-11 17:05:12 +1100

    Update help text

--- failrunner/cli.py
+++ failrunner/cli.py
@@ -9,9 +9,9 @@ def main():
     parser.add_argument('-j', '--job', metavar='J', type=int, nargs=1, help='Travis job number')
     parser.add_argument('-p', '--path', metavar='C', type=str, nargs=1,
                         help='Path to manage.py', default=path.abspath(path.dirname(__file__)))
-    parser.add_argument('-e', '--pipenv', action='store_true')
-    parser.add_argument('--fail-only', action='store_true')
-    parser.add_argument('--error-only', action='store_true')
+    parser.add_argument('-e', '--pipenv', action='store_true', help='Run manage.py through pipenv')
+    parser.add_argument('--fail-only', action='store_true', help='Run only failed tests')
+    parser.add_argument('--error-only', action='store_true', help='Run only errored tests')
     parser.add_argument('--dry', action='store_true', help='Print command to be run, but don\'t run it')
     parser.add_argument('--org', action='store_true', help='Use travis-ci.org instead of travis-ci.com')
     args = parser.parse_args()
@@ -31,6 +31,7 @@ def main():
     loaded = runner.get_tests(args.job[0], urltype)
     if not loaded:
         exit(1)
+
     runner.run_tests()
 
 
