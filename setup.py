511b356161efc0c9f1162a5feef778ab37152db7
Harry White <harry.white424@gmail.com>
2018-12-02 13:31:00 +1100

    Better setup, version bump

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
-
+from os import path
 from distutils.core import setup
+
+here = path.abspath(path.dirname(__file__))
+
+VERSION = '1.0.1'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='failrunner-django',
-    version='1.0.0',
+    version=VERSION,
     packages=['failrunner'],
-    description='',
+    description='A command line tool to run django tests that failed during a travis job',
+    long_description=long_description,
     author='Harry White',
     url='https://github.com/harrywhite4/failrunner-django',
     entry_points={
@@ -14,5 +23,6 @@ setup(
     },
     install_requires=[
         'requests'
-    ]
+    ],
+    python_requires='>=3.5'
 )
