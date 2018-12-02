82e317ffc375fa79ac893885875cbda0ddaca155
Harry White <harry.white424@gmail.com>
2018-12-02 13:14:58 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='failrunner-django',
-    version='0.1',
+    version='1.0.0',
     packages=['failrunner'],
     description='',
     author='Harry White',
